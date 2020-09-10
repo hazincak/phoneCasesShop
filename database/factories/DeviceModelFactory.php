52f3751ee99e8e3efca5868bcc03a8f8768ec30c
Jan Hazincak <jan.hazincak@gmail.com>
2020-09-10 15:07:19 +0100

    Categories, brands, device models controllers, models

---
+++ database/factories/DeviceModelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\DeviceModel;
+use Faker\Generator as Faker;
+
+$factory->define(DeviceModel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
